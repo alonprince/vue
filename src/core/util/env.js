96c836f84de7bedec21b7c28d547105fb4f30b20
Phishing <alonprince@hotmail.com>
2018-01-30 23:03:03 +0800

    test

--- src/core/util/env.js
+++ src/core/util/env.js
@@ -57,6 +57,9 @@ export function isNative (Ctor: any): boolean {
   return typeof Ctor === 'function' && /native code/.test(Ctor.toString())
 }
 
+/**
+ * 是否支持symbol
+ */
 export const hasSymbol =
   typeof Symbol !== 'undefined' && isNative(Symbol) &&
   typeof Reflect !== 'undefined' && isNative(Reflect.ownKeys)
